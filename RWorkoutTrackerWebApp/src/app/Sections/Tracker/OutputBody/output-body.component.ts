0310bcbbce429afe64fae4827b87e7001e9af515
metamorphic-rock <113041227+metamorphic-rock@users.noreply.github.com>
2023-02-14 21:40:20 +0800

    Added the muscle group to the output table

--- RWorkoutTrackerWebApp/src/app/Sections/Tracker/OutputBody/output-body.component.ts
+++ RWorkoutTrackerWebApp/src/app/Sections/Tracker/OutputBody/output-body.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SetItem } from 'src/app/Models/SetItemModel';
+import { ExerciseItem } from 'src/app/Models/ExerciseItemModel';
+import { ExerciseItemServicesService } from 'src/app/Services/exercise-item-services.service';
 import { SetItemServicesService } from 'src/app/Services/set-item-services.service';
 
 @Component({
@@ -8,7 +10,8 @@ import { SetItemServicesService } from 'src/app/Services/set-item-services.servi
   styleUrls: ['./output-body.component.scss']
 })
 export class OutputBodyComponent implements OnInit{
-  constructor(private setItemService:SetItemServicesService){}
+  constructor(private setItemService:SetItemServicesService,
+              private exerciseItemService:ExerciseItemServicesService){}
   enableSetEdit:boolean=false;
   @Input() set: SetItem={
     'id':0,
@@ -18,7 +21,14 @@ export class OutputBodyComponent implements OnInit{
     'weight':0,
     'reps':0
   }
+  exercise!: ExerciseItem;
+  exerciseId:number=Number(this.set.exerciseId)
   ngOnInit(): void {  
+    this.exerciseItemService.GetExerciseByIdFromDB(Number(this.set.exerciseId)).subscribe(e=>{
+      this.exercise=e
+      console.log(this.set.exerciseId)
+      console.log(this.exercise)
+    })
   }
   @Output() editSetEvent : EventEmitter<any>=new EventEmitter<any>();
   EditSet=()=>{
@@ -38,7 +48,7 @@ export class OutputBodyComponent implements OnInit{
   };
   SaveChanges=()=>{
     this.setItemService.SaveSetToDB(this.set).subscribe()
-    console.log("edit set,id : ")
+    console.log("edit set id : ")
     console.log(this.set.id)
   }
 }
